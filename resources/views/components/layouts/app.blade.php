40024e33fb7fe599e667652086c505cfbaa46a12
vikashaaryan <vikasharyan323@gmail.com>
2025-06-22 12:06:17 +0530

    livewire problam solve

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    @livewireStyles
 </head>
 
 <body>
@@ -23,6 +24,7 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    @livewireScripts
 </body>
 
 </html>
